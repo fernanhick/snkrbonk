4d66ce8b1fdebce884ba3c81e9caef1bb6cecc7d
fernanhick <fernanhick@gmail.com>
2026-01-20 10:17:24 +0000

    config nativewind

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
+    <SafeAreaView
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
       }}
+      className="bg-blue-800"
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    </SafeAreaView>
   );
 }
